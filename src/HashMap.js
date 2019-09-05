2ab85a8c454be64e043c4753da1cd913cdffba81
Todd Davis <tejastcd@gmail.com>
2019-09-05 08:26:38 -0400

    practice

--- src/HashMap.js
+++ src/HashMap.js
@@ -1,5 +1,5 @@
 class HashMap {
-    constructor(initialCapacity=8) {
+    constructor(initialCapacity = 8) {
         this.length = 0;
         this._hashTable = [];
         this._capacity = initialCapacity;
@@ -14,7 +14,7 @@ class HashMap {
         return this._hashTable[index].value;
     }
 
-    set(key, value){
+    set(key, value) {
         const loadRatio = (this.length + this._deleted + 1) / this._capacity;
         if (loadRatio > HashMap.MAX_LOAD_RATIO) {
             this._resize(this._capacity * HashMap.SIZE_RATIO);
@@ -22,7 +22,7 @@ class HashMap {
         //Find the slot where this key should be in
         const index = this._findSlot(key);
 
-        if(!this._hashTable[index]){
+        if (!this._hashTable[index]) {
             this.length++;
         }
         this._hashTable[index] = {
@@ -47,7 +47,7 @@ class HashMap {
         const hash = HashMap._hashString(key);
         const start = hash % this._capacity;
 
-        for (let i=start; i<start + this._capacity; i++) {
+        for (let i = start; i < start + this._capacity; i++) {
             const index = i % this._capacity;
             const slot = this._hashTable[index];
             if (slot === undefined || (slot.key === key && !slot.DELETED)) {
@@ -86,3 +86,40 @@ class HashMap {
         return hash >>> 0;
     }
 }
+
+const WhatDoesThisDo = function () {
+    let str1 = 'Hello World.';
+    let str2 = 'Hello World.';
+    let map1 = new HashMap();
+    map1.set(str1, 10);
+    map1.set(str2, 20);
+    let map2 = new HashMap();
+    let str3 = str1;
+    let str4 = str2;
+    map2.set(str3, 20);
+    map2.set(str4, 10);
+
+    console.log('key str1: ' + map1.get(str1));
+    console.log('key str1: ' + map2.get(str3));
+}
+
+// Answer: demonstrates how no duplicate keys are allowed. Last value key/pair overwrites existing key/pair
+
+function permutationPalindrome(str) {
+    if (!str || str === '') {
+        return false
+    }
+
+    const map = new HashMap();
+    for (let i = 0; i < str.length; i++) {
+        if(map.get('c')){
+            //map.set(str.charAt(i), 1)
+            return false
+        }
+    }
+
+   return map.length
+
+}
+
+console.log(permutationPalindrome('acecarr'))
